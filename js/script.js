9b811285926b3484c88676bbeebc49d44205c382
DiegoValdivieso22 <159164145+DiegoValdivieso22@users.noreply.github.com>
2024-04-03 21:14:50 +0000

    edit

--- js/script.js
+++ js/script.js
@@ -1,5 +1,28 @@
-// Copyright (c) 2020 Mr. Coxall All rights reserved
-//
-// Created by: Mr. Coxall
-// Created on: Sep 2020
-// This file contains the JS functions for index.html
+function doMathClicked() {
+  // this function does basic math: Mph to Kph
+  let Mph = document.getElementById("Mph").value;
+  let answer = Mph * 1.60;
+
+  // Make sure not negative
+  if (answer < 0) {
+    answer = 0;
+    document.getElementById("Kp").innerHTML = "Invalid input. Ensure value is a positive number.";
+  }
+  else {
+    document.getElementById("Kph").innerHTML = "Kph = " + answer.toFixed(2);
+  }
+}
+function kphToMetersPH() {
+  let kph = document.getElementById("Kph").value;
+  let calculation = kph * 1000;
+
+  // Make sure not negative
+  if (calculation <= 0) {
+    calculation = 0;
+    document.getElementById("metersperhour").innerHTML = "Invalid input. Ensure value is a positive number.";
+  }
+  else {
+    document.getElementById("metersperhour").innerHTML = "Meters per hour is = " + calculation.toFixed(2);
+
+  }
+}
